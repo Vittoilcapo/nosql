ea26e4d1b4b9693b936af59bc977787dd1fb4e0a
nicosposito <nicolas.sposito16@hotmail.com>
2022-11-07 04:43:13 -0300

    Añadido archivos para dockerizar solucion

--- src/main/java/com/nosql/nosql/configuration/RedisConfig.java
+++ src/main/java/com/nosql/nosql/configuration/RedisConfig.java
@@ -3,6 +3,7 @@ package com.nosql.nosql.configuration;
 import com.nosql.nosql.clases.Usuario;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
@@ -13,6 +14,11 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 public class RedisConfig {
     @Bean
     JedisConnectionFactory jedisConnectionFactory() {
+
+        /*Docker config
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration("redis", 6379);
+        return new JedisConnectionFactory(redisStandaloneConfiguration);
+         */
         return new JedisConnectionFactory();
     }
 
